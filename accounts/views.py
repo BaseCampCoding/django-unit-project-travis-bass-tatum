30a7c13c5033239e907beb17966f0863034031b2
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-03-09 15:23:26 -0600

    here's what I have so far.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
-from accounts.forms import RegisterForm
+from accounts.forms import RegistrationForm
 
 def register_view(request, *args, **kwargs):
     user = request.user
@@ -9,7 +9,7 @@ def register_view(request, *args, **kwargs):
         return HttpResponse(f"You are already authenicated as {user.email}.")
     context = {}
     if request.POST:
-        form = RegisterForm(request.POST):
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             email = form.cleaned_data.get('email').lower()
