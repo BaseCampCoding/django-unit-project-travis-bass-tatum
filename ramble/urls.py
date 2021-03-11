970802deb12a4bfbbf52eaf32ce28057eeb497b3
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2021-03-11 10:23:10 -0600

    Added update and delete views

--- ramble/urls.py
+++ ramble/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from .views import(
     UserListView, 
     UserDetailView,
+    UserUpdateView,
+    UserDeleteView,
 )
 
 urlpatterns = [
+    path('<int:pk>/edit/',
+        UserUpdateView.as_view(), name='user_edit'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('<int:pk>/delete/',
+        UserDeleteView.as_view(), name='user_delete'),
     path('', UserListView.as_view(), name='user_list'),
 ]
