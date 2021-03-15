3e09eed954a046bcb82b4070c10f8c3bdf145fec
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-03-15 15:03:46 -0500

    4 more public rooms

--- ramble/routing.py
+++ ramble/routing.py
@@ -5,4 +5,8 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/(chat/public1\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/(chat/public2\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/(chat/public3\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/(chat/public4\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/(chat/public5\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
