59525c4fc35dc3eb83d08645b1bac34495bca3a7
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2021-03-16 10:39:43 -0500

    Pushing comments

--- ramble/admin.py
+++ ramble/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
 
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
