c1e041f63465ebd2394b1811f99ff0ff06d1b788
Jeremiah <72397368+BlueBass1@users.noreply.github.com>
2021-03-16 11:33:51 -0500

    fixed fix

--- ramble/models.py
+++ ramble/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-from posts.models import Post
 
 # Create your models here.
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, 
-        on_delete=models.CASCADE,
-        related_name='comments',
-    )
-    comment = models.CharField(max_length=250)
-    author = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE,
-    )
 
-    def __str__(self):
-        return self.comment
-    
-    def get_absolute_url(self):
-        return reverse("user_list")
-    
     
