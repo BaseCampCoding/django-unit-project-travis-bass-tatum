02e461d46e5482e7a725e6cd8b5b587890dd95b4
Travis Alexander <talexander21@basecampcodingacademy.org>
2021-03-17 15:21:56 -0500

    back to dev mode

--- config/settings.py
+++ config/settings.py
@@ -151,11 +151,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ASGI_APPLICATION = 'config.asgi.application'
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
